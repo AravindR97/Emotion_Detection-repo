af12d08f84da947399ab920214ef0836cf7da764
AravindR97 <132665408+AravindR97@users.noreply.github.com>
2023-12-01 19:55:30 +0530

    container url correction

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,8 +5,8 @@ showing various emotions and their scores after emotion analysis
 import requests
 
 def emotion_detector(text_to_analyze):
-    url = 'https://e3ed180236.dsceapp.buildlab.cloud/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    #url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    #container_url = 'Paste the saved container url here!'
+    url = container_url + '/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     head =  {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     data = { "raw_document": { "text": text_to_analyze } }
 
